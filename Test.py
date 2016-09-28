4d16d162f9d61528444ada6d2c155193a1a933b4
Kell <ppham@csumb.edu>
2016-09-28 11:44:54 -0700

    Put everything into methods

--- Test.py
+++ Test.py
@@ -6,30 +6,43 @@
 
 import math
 
-#variable from dictionary
-x1 = (434,546)
-x2 = (31,245)
-
-
-def distance(x1, x2):
-	return math.sqrt((x1[0] - x2[0])**2 + (x1[1] - x2[1])**2)	
+class mathing():
+	D = 0.0
+	F = 0.0
+	W = 0.0
+	P = 0.0
+	D1 = 0.0
 	
-
-D = float(input("Enter distance from camera in inches: "))
-W = float(input("Enter width of object: "))	
+	#variable from dictionary
+	x1 = (434,546)
+	x2 = (31,245)
 	
-P = distance(x1, x2)
-F = ((D * P) / W)
-
-"""def cdistance():
-	P = distance(x1, x2)
-	D1 = (( F * W)/ P)"""
+	def distance(self, x1, x2):
+		self.P = math.sqrt((x1[0] - x2[0])**2 + (x1[1] - x2[1])**2)	
 	
-D1 = (( F * W)/ P)	
+	def input(self):
+		self.D = float(input("Enter distance from camera in inches: "))
+		self.W = float(input("Enter width of object: "))	
+		
+	def calculatefocal(self):
+		self.F = ((self.D * self.P) / self.W)
+	
+	def calculatedistance(self):
+		self.D1 = ((self.F * self.W)/ self.P)	
 
-print("Distance: ", D1)
-print ("Set Distance: %.2f" % (P))
-print("Width of object is: ", W)
-print("Focal length is: %.2f " % (F))
+	def print(self):
+		print("Input Distance", self.D)
+		print("Input Width", self.W)
+		print("Pixel Distance", self.P)
+		print("Focal Length", self.F)
+		
+	#def __init__(self):
 
-#output the screen 
+
+	
+math1 = mathing()	
+math1.distance(math1.x1, math1.x2)		
+math1.input()	
+math1.calculatefocal()
+math1.calculatedistance()	
+math1.print()
